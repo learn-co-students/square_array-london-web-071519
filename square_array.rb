5334839dbe0442b625e2d598597baa78f0385b59
Eliot Howes <ejhowes@gmail.com>
2019-05-23 22:10:02 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,8 +1,5 @@
 def square_array(array)
   # your code here
-  new_numbers = []
-  array.each do |number|
-    new_numbers.push(number * number)
-  end
-  return new_numbers
+
+  array.collect {|number| number * number}
 end
