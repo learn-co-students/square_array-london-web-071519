42b6c2c94c3623148eca32c7afef7dddf84d68a8
Eliot Howes <ejhowes@gmail.com>
2019-05-23 22:22:13 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,6 +1,11 @@
 def square_array(array)
   # your code here
   new_array = []
-  array.each {|number| new_array.push(Math.sqrt(number))}
+  array.each {|number| new_array.push(number ** 2)}
   return new_array
 end
+
+# def square_array(array)
+#   # your code here
+#   array.collect {|number| number ** 2}
+# end
